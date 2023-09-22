3ba26c39faf42b90d10f4ef5038b009cd0c5fa81
Giovanni Brasil <giovannisbrasil@gmail.com>
2023-09-22 12:17:37 -0300

    Tabuada Js

---
+++ JsBasic/Tabuada.js
@@ -0,0 +1,18 @@
+/*
+const multiplicador = 7;
+
+for (let k=0; k <=10; k++) {
+    const calc = multiplicador *  k;
+    console.log(`${multiplicador} x ${k} = ${calc}`);
+}
+
+console.log("///////////////////////////////////////////")
+*/
+for (let i=1; i<=10; i++) {
+    console.log("TABUADA DO: " + i);
+    for (let k=0; k <=10; k++) {
+        const calc = i *  k;
+        console.log(`${i} x ${k} = ${calc}`);
+    }
+    console.log("")
+}
