ec95cf72fcb9035955837dd9d226cd9c422f77ad
Giovanni Brasil <giovannisbrasil@gmail.com>
2023-09-22 13:38:17 -0300

    Strings

---
+++ JsBasic/String.js
@@ -0,0 +1,18 @@
+// Elaborar um código que conte a quantidade de cada letra numa palavra
+
+const palavra = "Analoga".toLowerCase(); // passando todas as letras para minusculo // declarando a palavra a ser analisada
+
+let letras = {}; // criando um array para guardar o resultado da contagem de letras
+
+// O array em JS funciona similar ao Dicionario do Python, ele tem uma chave e um valor
+// a iteração for procura verificar se existe letras[palavra[0]] = letras[a], e o JS interpreta que tu esta buscando a chave "a" no array letras
+// Como incialmente não existe, a gnt cria essa chave na condição ELSE dizendo que letras[a] = 1, assim a chave é "a" e o valor é 1
+// Na proxima vez que encontrar a letra "a" na palavra, ele vai verificar que já existe a chave "a" no array 'letras' e vai fazer letras[a] = +1;
+for (let i=0; i< palavra.length; i++) {
+    if (letras[palavra[i]]) {
+        letras[palavra[i]]++;
+    } else {
+        letras[palavra[i]] = 1;
+    }
+}console.log(`A palavra ${palavra} tem ${palavra.length} letras. E a quantidade de cada uma é apresentada abaixo:`)
+console.log(letras);
